3b9e1e0b679d3968c6f6031049c3db9c7507a3c5
edpackard <edpackard81@gmail.com>
2021-09-15 17:30:16 +0100

    adding old school hbs - working!

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 
 const express = require("express");
+const path = require("path");
 const cookieParser = require("cookie-parser");
+
 const app = express();
 const port = 3000;
-const path = require("path");
+
 // const hbs = require("hbs");
 
 const postsRouter = require("./routes/posts");
@@ -18,11 +20,11 @@ app.use(cookieParser());
 
 // route setup
 app.get("/", (request, response) => {
-	response.json({ info: "Node.js, Express, and Postgres API" });
+  response.json({ info: "Node.js, Express, and Postgres API" });
 }); // this will need homeRouter
 
 app.use("/posts", postsRouter);
 
 app.listen(port, () => {
-	console.log(`App running on port ${port}.`);
+  console.log(`App running on port ${port}.`);
 });
