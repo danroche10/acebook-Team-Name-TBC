f90a5220308d8c576103731a33d275f70ac09bc6
edpackard <edpackard81@gmail.com>
2021-09-15 07:04:09 +0100

    alternative method returning hashes

--- model/posts.js
+++ model/posts.js
@@ -1,18 +1,13 @@
 const connection = require("../database/connection.js");
 
 class Post {
-  constructor(id, message) {
-    this.id = id;
-    this.message = message;
-  }
-
   static async getPosts() {
     let allPostsArray = [];
     let allPosts = await connection.pool.query(
       "SELECT * FROM posts ORDER BY id ASC"
     );
     allPosts.rows.forEach((element) => {
-      allPostsArray.push(new Post(element.id, element.message));
+      allPostsArray.push({ id: element.id, message: element.message });
     });
     return allPostsArray;
   }
