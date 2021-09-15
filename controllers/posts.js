b0bc1746a11c7897eac9a7a52c270c90fd9fb1a6
edpackard <edpackard81@gmail.com>
2021-09-15 09:24:14 +0100

    minor tweaking

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,7 +3,6 @@ const Post = require("../model/posts");
 const PostsController = {
   Index: async function (req, res) {
     let posts = await Post.getPosts();
-    // console.log({posts; posts});
     res.send({ posts: posts });
   },
 };
