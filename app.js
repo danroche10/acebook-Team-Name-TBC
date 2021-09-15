ff4914fc733b48b150a533bd9d51afb8cf61d1e6
Ahsan <rasul.ahsan@gmail.com>
2021-09-15 16:55:03 +0100

    having issues rendering posts/index

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
 const port = 3000;
+const path = require("path");
+// const hbs = require("hbs");
 
 const postsRouter = require("./routes/posts");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -13,11 +18,11 @@ app.use(cookieParser());
 
 // route setup
 app.get("/", (request, response) => {
-  response.json({ info: "Node.js, Express, and Postgres API" });
+	response.json({ info: "Node.js, Express, and Postgres API" });
 }); // this will need homeRouter
 
 app.use("/posts", postsRouter);
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`);
+	console.log(`App running on port ${port}.`);
 });
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router();
 
+console.log("posts.js before route")
 const PostsController = require('../controllers/posts')
 router.get('/', PostsController.Index);
-
+console.log("posts.js after route")
 module.exports = router;
 
