a9445590333cce839e8d1006ccf31f827b2fc066
edpackard <edpackard81@gmail.com>
2021-09-16 11:11:44 +0100

    setting up eslint to work with prettier

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
-const cookieParser = require("cookie-parser");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
 const app = express();
 const port = 3000;
 
-const postsRouter = require("./routes/posts");
+const postsRouter = require('./routes/posts');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // route setup
-app.get("/", (request, response) => {
-  response.json({ info: "Node.js, Express, and Postgres API" });
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' });
 }); // this will need homeRouter
 
-app.use("/posts", postsRouter);
+app.use('/posts', postsRouter);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
