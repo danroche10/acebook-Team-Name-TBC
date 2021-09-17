df2828b9b6d43008a323cf3f832d9077b1e8a18d
zainab baruud <zainabbaruud@ma373.home>
2021-09-17 11:52:57 +0100

    ZB & EP: adding automation to pool.end()

--- spec/db.test.js
+++ spec/db.test.js
@@ -1,9 +1,12 @@
 const { pool } = require('../database/connection'); // this is essential if you are testing anything to do with the database
 const Post = require('../model/posts');
 
+afterEach(() => {
+  pool.end();
+});
+
 test('checks test db is accessed', () => {
   expect(Post.getPosts()).resolves.toStrictEqual([
     { id: 1, message: 'testtesttest' },
   ]);
-  pool.end();
 });
