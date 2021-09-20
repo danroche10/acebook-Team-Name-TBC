c8e7d8349b8e5a206f53d18f2afd3a534dd94741
edpackard <edpackard81@gmail.com>
2021-09-20 12:15:36 +0100

    finishing initial cypress test for reverse posts

--- cypress/integration/reversePosts.spec.js
+++ cypress/integration/reversePosts.spec.js
@@ -1,5 +1,10 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    cy.visit('http://localhost:3000/posts');
+describe('Posts page', () => {
+  it('visits post page and checks title', () => {
+    cy.visit('/posts');
+    cy.get('h1').should('contain', 'Posts');
   });
 });
+
+// this test can be improved when front end ability to add posts is added
+// i.e. can set up test with two posts and then expect posts to appear in reverse
+// chronological order
