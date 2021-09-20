194e8629b176a19b8437a2362a165138030e360e
danroche10 <danielroche10@gmail.com>
2021-09-20 12:26:12 +0100

    sign cypress test running on browser

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -1,11 +1,14 @@
-// <reference types="cypress" />
+/// <reference types="cypress" />
 
 describe('The Signup Page', () => {
-
-    // beforeEach(() => {
-    // })
-    it('contains sign up form', () => {
-        cy.visit('http://localhost:3000/signup')
-        cy.contains("Welcome to Acebook Sign Up Page") 
-    })
-  })
+  // beforeEach(() => {
+  // })
+  it('contains sign up form', () => {
+    cy.visit('http://localhost:3000/signup');
+    cy.get('#newHandle').type('blahdeblah');
+    cy.get('#newPassword').type('blahdeblah');
+    cy.get('#newEmail').type('blah@blah.com');
+    cy.get('#newUser').submit();
+    cy.contains('Posts');
+  });
+});
