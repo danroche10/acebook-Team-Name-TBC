09af43a4e2a14fa60e2c43033f558a1cca6e398a
danroche10 <danielroche10@gmail.com>
2021-09-21 16:53:32 +0100

    DR & HE: catching Login error

--- controllers/home.js
+++ controllers/home.js
@@ -6,28 +6,32 @@ const HomeController = {
     res.render('home/index');
   },
   async Login(req, res) {
-    const username = req.body.user_name;
-    const password = req.body.user_password;
-    const isUserAuthenticated = await User.authenticate(username, password);
-    const userId = isUserAuthenticated.id;
-    if (isUserAuthenticated === false) {
-      res.redirect('/');
+    try {
+      const username = req.body.user_name;
+      const password = req.body.user_password;
+      const isUserAuthenticated = await User.authenticate(username, password);
+      const userId = isUserAuthenticated.id;
+      if (isUserAuthenticated === false) {
+        res.redirect('/');
+      }
+      // Add your authenticated property below:
+      req.session.authenticated = true;
+      // Add the user object below:
+      req.session.user = {
+        username,
+        userId,
+      };
+      // console.log(req.session);
+      // below is short-term solution until we can properly use express-session
+      res.redirect(
+        url.format({
+          pathname: '/posts',
+          query: req.session.user,
+        })
+      );
+    } catch (error) {
+      console.log('error');
     }
-    // Add your authenticated property below:
-    req.session.authenticated = true;
-    // Add the user object below:
-    req.session.user = {
-      username,
-      userId,
-    };
-    // console.log(req.session);
-    // below is short-term solution until we can properly use express-session
-    res.redirect(
-      url.format({
-        pathname: '/posts',
-        query: req.session.user,
-      })
-    );
   },
 };
 module.exports = HomeController;
