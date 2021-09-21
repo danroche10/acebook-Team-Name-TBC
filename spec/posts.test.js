9ade1fb9dc3c0ec3b5bfb36052def574dd17a8db
zainab baruud <zainabbaruud@ma373.home>
2021-09-21 11:32:10 +0100

    ZB & AR: testing for retrieving a single post

--- spec/posts.test.js
+++ spec/posts.test.js
@@ -1,3 +1,4 @@
+const { test } = require('mocha');
 const PostsController = require('../controllers/posts');
 const { pool } = require('../database/connection'); // this is essential if your tests involve database connection
 const Post = require('../model/posts'); // put whatever you are testing here
@@ -22,4 +23,16 @@ test('checks post can be added', async () => {
   expect(data).toStrictEqual([
     { id: 1, message: 'This is testing adding a post' },
   ]);
+
+test('checks a single post is retrieved', async() => {
+  await pool.query(
+    "INSERT INTO users(username, password, email) VALUES('dandelion', 'Password1', 'test@test.com');"
+  )
+  await Post.addPost('This is our test retrieving post', 1); 
+  await Post.addPost('This is our second test retrieving post', 1);
+  const post_id = 1 
+  const post = await Post.getPostById(post_id)
+  expect(post).toStrictEqual([
+    {id: 1, message: 'This is our test retrieving post'}])
+})
 });
