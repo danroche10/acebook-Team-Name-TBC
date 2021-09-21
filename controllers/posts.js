c6b51b3bdb595cff1a254d657711b61160bf93d2
Ahsan <rasul.ahsan@gmail.com>
2021-09-21 10:26:17 +0100

    changed how to select an individual post from a static href to an on click event

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,6 @@
 const { request } = require('express');
 const Post = require('../model/posts');
+const url = require('url');
 
 const PostsController = {
   //  let's change away from req, res as this is old syntax (I think)
@@ -8,19 +9,22 @@ const PostsController = {
     res.render('posts/index', { posts: posts });
   },
   Show: async function (req, res) {
-    res.json({ info: 'Hello show posts router :)' });
+    // console.log(req.url);
+    post_id = req.url;
+    post_id = post_id.split('/')[1];
+    // console.log(post_id);
+    res.json({ info: 'Hello show posts router :)', post_id });
   },
   New: async function (req, res) {
     try {
-    // temporary workaround till user login - req.body.user_id
-    await Post.addPost(req.body.text, req.body.user_id);
-    res.redirect(302, 'back'); // could use res.redirect('back') as it does the same
+      // temporary workaround till user login - req.body.user_id
+      await Post.addPost(req.body.text, req.body.user_id);
+      res.redirect(302, 'back'); // could use res.redirect('back') as it does the same
     } catch (error) {
       console.log(error);
     }
   },
   NewComment: async function (req, res) {
-    
     res.json({ info: 'Hello new comment post router :)' });
   },
   NewLike: async function (req, res) {
