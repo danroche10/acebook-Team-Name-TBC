de4cb1f1b279e53e32433003bc26a0714790b3da
zainab baruud <zainabbaruud@ma373.home>
2021-09-21 16:32:27 +0100

    ZB & AR: Messing around with CSS

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const signupRouter = require("./routes/signup");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -27,3 +28,4 @@ app.use("/signup", signupRouter);
 app.listen(port, () => {
 	console.log(`App running on port ${port}.`);
 });
+
